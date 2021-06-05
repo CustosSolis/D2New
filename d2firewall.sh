f66feb473c01d2217b31a266229f087a4cde7355
Fil93z <83925286+Fil93z@users.noreply.github.com>
2021-06-05 13:08:50 +0200

    Add files via upload

--- d2firewall.sh
+++ d2firewall.sh
@@ -89,6 +89,37 @@ install_dependencies () {
 setup () {
   echo "Setting up firewall rules."
   reset_ip_tables
+  sudo iptables -A INPUT -s 103.10.124.99 -j DROP
+  sudo iptables -A INPUT -s 103.10.125.146 -j DROP
+  sudo iptables -A INPUT -s 139.45.193.10 -j DROP
+  sudo iptables -A INPUT -s 146.66.154.35 -j DROP
+  sudo iptables -A INPUT -s 146.66.155.52 -j DROP
+  sudo iptables -A INPUT -s 155.133.230.67 -j DROP
+  sudo iptables -A INPUT -s 155.133.232.98 -j DROP
+  sudo iptables -A INPUT -s 155.133.233.99 -j DROP
+  sudo iptables -A INPUT -s 155.133.235.18 -j DROP
+  sudo iptables -A INPUT -s 155.133.235.34 -j DROP
+  sudo iptables -A INPUT -s 155.133.238.162 -j DROP
+  sudo iptables -A INPUT -s 155.133.239.59 -j DROP
+  sudo iptables -A INPUT -s 155.133.245.34 -j DROP
+  sudo iptables -A INPUT -s 155.133.246.50 -j DROP
+  sudo iptables -A INPUT -s 155.133.249.194 -j DROP
+  sudo iptables -A INPUT -s 155.133.250.130 -j DROP
+  sudo iptables -A INPUT -s 155.133.252.51 -j DROP
+  sudo iptables -A INPUT -s 155.133.253.4 -j DROP
+  sudo iptables -A INPUT -s 155.133.254.138 -j DROP
+  sudo iptables -A INPUT -s 162.254.192.70 -j DROP
+  sudo iptables -A INPUT -s 162.254.193.101 -j DROP
+  sudo iptables -A INPUT -s 162.254.195.86 -j DROP
+  sudo iptables -A INPUT -s 162.254.196.66 -j DROP
+  sudo iptables -A INPUT -s 162.254.198.103 -j DROP
+  sudo iptables -A INPUT -s 162.254.199.178 -j DROP
+  sudo iptables -A INPUT -s 185.25.183.178 -j DROP
+  sudo iptables -A INPUT -s 190.217.33.66 -j DROP
+  sudo iptables -A INPUT -s 205.185.194.36 -j DROP
+  sudo iptables -A INPUT -s 205.196.6.74 -j DROP
+  sudo iptables -A INPUT -s 205.209.16.137 -j DROP
+  sudo iptables -A INPUT -s 205.234.119.194 -j DROP
 
   read -p "Enter your platform xbox, psn, steam: " platform
   platform=$(echo "$platform" | xargs)
